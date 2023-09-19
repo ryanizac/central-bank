e82132a7fe5ff31f1a99546a230dd2fd8aa4cf17
Ryan Izac <io.ryan@outlook.com>
2023-09-19 00:12:09 -0300

    feat: create bank use case

---
+++ src/data/usecases/ports/bank-repository.ts
@@ -0,0 +1,21 @@
+import { Bank } from "../../../domain";
+
+export type BankRepository = {
+  exists(args: BankRepository.ExistsArgs): Promise<BankRepository.ExistsResult>;
+  create(data: BankRepository.Data): Promise<void>;
+};
+
+export namespace BankRepository {
+  export type ExistsArgs = Partial<{
+    code: string;
+    email: string;
+    name: string;
+    shortname: string;
+    cpf: string;
+    cnpj: string;
+  }>;
+
+  export type ExistsResult = Partial<Record<keyof ExistsArgs, boolean>>;
+
+  export type Data = Bank;
+}
---
+++ src/data/usecases/register-bank.ts
@@ -0,0 +1,86 @@
+import { CreateBank } from "../../domain";
+import { BankRepository, IdGenerator } from "./ports";
+
+export class RegisterBankUseCase implements CreateBank {
+  private readonly idGenerator: IdGenerator;
+  private readonly bankRepository: BankRepository;
+
+  constructor({ idGenerator, bankRepository }: RegisterBankUseCase.Ports) {
+    this.idGenerator = idGenerator;
+    this.bankRepository = bankRepository;
+  }
+
+  async execute(args: CreateBank.Args): Promise<CreateBank.Result> {
+    const { code, email, name, shortname, password, account } = args;
+
+    let cnpj: string | undefined = undefined,
+      cpf: string | undefined = undefined;
+
+    if (account.type === "business") {
+      cnpj = account.cnpj;
+    } else {
+      cpf = account.cpf;
+    }
+
+    const dataInUse = await this.bankRepository.exists({
+      cnpj,
+      cpf,
+      code,
+      email,
+      name,
+      shortname,
+    });
+
+    const argsInUse: string[] = [];
+
+    for (const key in dataInUse) {
+      if (dataInUse[key as keyof typeof dataInUse] === true) {
+        argsInUse.push(key);
+      }
+    }
+
+    if (argsInUse.length > 0) {
+      const keys = argsInUse.join(", ");
+      throw new Error("The following data is already in use: " + keys);
+    }
+
+    const createdAt = new Date(),
+      updatedAt = createdAt,
+      active = false,
+      id = this.idGenerator.generate();
+
+    await this.bankRepository.create({
+      id,
+      createdAt,
+      updatedAt,
+      active,
+      name,
+      shortname,
+      code,
+      email,
+      account,
+      password,
+    });
+
+    return {
+      bank: {
+        id,
+        createdAt,
+        updatedAt,
+        active,
+        name,
+        shortname,
+        code,
+        email,
+        account,
+      },
+    };
+  }
+}
+
+export namespace RegisterBankUseCase {
+  export type Ports = {
+    idGenerator: IdGenerator;
+    bankRepository: BankRepository;
+  };
+}
