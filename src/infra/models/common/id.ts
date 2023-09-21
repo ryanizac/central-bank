ca9b6e192b843f706e0084f0465cf21881b82988
Ryan Izac <io.ryan@outlook.com>
2023-09-21 14:52:11 -0300

    feat: define id metadata in id decorator

--- src/infra/models/common/id.ts
+++ src/infra/models/common/id.ts
@@ -1,3 +1,10 @@
+import { ModelMetadata } from "./metadata";
+
 export function Id<Proto extends object, Attribute extends keyof Proto>() {
-  return function (ClassPrototype: Proto, attribute: Attribute) {};
+  return function (ClassPrototype: Proto, attribute: Attribute) {
+    ModelMetadata.setId({
+      ClassPrototype,
+      attribute,
+    });
+  };
 }
--- src/infra/models/common/metadata.ts
+++ src/infra/models/common/metadata.ts
@@ -7,6 +7,17 @@ export class ModelMetadata {
   static setModel({ Class, name }: ModelMetadata.SetModelOptions) {
     this.metadata.set(Class, "model", name);
   }
+
+  static setId<Proto extends object>({
+    ClassPrototype,
+    attribute,
+  }: ModelMetadata.SetIdOptions<Proto>) {
+    if (typeof attribute !== "string") {
+      throw new Error("The id attribute must be a string key");
+    }
+
+    this.metadata.set(ClassPrototype, "id", attribute);
+  }
 }
 
 export namespace ModelMetadata {
@@ -15,6 +26,10 @@ export namespace ModelMetadata {
      * The name of model
      */
     model: string;
+    /**
+     * The attribute that represents id
+     */
+    id: string;
   };
 
   export type Class = {
@@ -24,5 +39,10 @@ export namespace ModelMetadata {
   export type SetModelOptions = {
     Class: Class;
     name: string;
+  };
+
+  export type SetIdOptions<Proto extends object> = {
+    ClassPrototype: Proto;
+    attribute: keyof Proto;
   };
 }
