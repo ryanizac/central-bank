b7e67da3ca75aa251dc35878715abe7cfa0634df
Ryan Izac <io.ryan@outlook.com>
2023-09-22 11:44:19 -0300

    feat: add callback support to set metadata

--- src/shared/metadata.ts
+++ src/shared/metadata.ts
@@ -1,7 +1,29 @@
 import "reflect-metadata";
 
 export class Metadata<Data extends object> {
-  set<K extends keyof Data>(Class: object, key: K, value: Data[K]) {
+  private isFunction<T>(value: any): value is Metadata.CallbackSet<T> {
+    return typeof value === "function";
+  }
+
+  set<K extends keyof Data>(
+    Class: object,
+    key: K,
+    valueOrCallbackSet: Metadata.SetValue<Data[K]>,
+  ) {
+    let value: Data[K];
+
+    if (this.isFunction<Data[K]>(valueOrCallbackSet)) {
+      let currentValue = Reflect.getMetadata(key, Class);
+
+      if (currentValue === undefined) {
+        currentValue = [];
+      }
+
+      value = valueOrCallbackSet(currentValue);
+    } else {
+      value = valueOrCallbackSet;
+    }
+
     Reflect.defineMetadata(key, value, Class);
   }
 
@@ -9,4 +31,10 @@ export class Metadata<Data extends object> {
     const value = Reflect.getMetadata(key, Class);
     return value;
   }
+}
+
+export namespace Metadata {
+  export type CallbackSet<T> = (prev: T) => T;
+
+  export type SetValue<T> = T | CallbackSet<T>;
 }
